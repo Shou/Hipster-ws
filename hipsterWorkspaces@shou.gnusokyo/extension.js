6c7c1b1df9dc5efca68eecaa2d524f910f488fb2
Shou <gnusokyo@gmail.com>
2013-07-28 15:42:07 +0200

    modifies existing workspaces instead of creating new ones

--- hipsterWorkspaces@shou.gnusokyo/extension.js
+++ hipsterWorkspaces@shou.gnusokyo/extension.js
@@ -25,102 +25,125 @@
 
 // {{{ Imports
 
-const Lang = imports.lang;
-const Main = imports.ui.main;
-const Meta = imports.gi.Meta;
-const PanelMenu = imports.ui.panelMenu;
-const Shell = imports.gi.Shell;
-const St = imports.gi.St;
+const Lang = imports.lang
+const Main = imports.ui.main
+const Meta = imports.gi.Meta
+const PanelMenu = imports.ui.panelMenu
+const Shell = imports.gi.Shell
+const St = imports.gi.St
 
 // }}}
 
 // TODO
 // - Settings
 //      - Colours.
+//      - Font.
 // - Default colours.
-// - Listen to URGENT windows and glow workspace.
+// - Listen to URGENT windows and glow (not flash) workspace.
 // - Clickable and scrollable workspaces.
+
+// {{{ Utils
+
+function or(xs) {
+    for (var i = 0; i < xs.length; i++) if (xs[i]) return true
+    return false
+}
+
+function map(f, xs) {
+    var tmp = []
+    for (var i = 0; i < xs.length; i++) tmp.push(f(xs[i]))
+    return tmp
+}
+
+// }}}
+
 
 // | classes can go to hell!!!
 // Workspaces :: IO Widget
 function Workspaces(){
-    let btn = new PanelMenu.Button;
-    let topBox = new St.BoxLayout();
-    let wscounter = global.screen.n_workspaces;
+    let btn = new PanelMenu.Button
+    let topBox = new St.BoxLayout()
+    let wscounter = global.screen.n_workspaces
 
     log("-- hipster: wscounter: " + wscounter)
 
     for (let i = 0; i < wscounter; i++) {
-        let className;
+        let className = "hipster-ws"
         if (global.screen.get_active_workspace_index() == i)
-            className = "hipster-ws-current";
-        else className = "hipster-ws";
+            className += " hipster-ws-current"
 
-        let con = new St.Bin({ style_class: className });
-        let txt = Meta.prefs_get_workspace_name(i);
-        let lab = new St.Label({ text: txt });
-        con.set_child(lab);
+        let con = new St.Bin({ style_class: className })
+        let txt = Meta.prefs_get_workspace_name(i)
+        let lab = new St.Label({ text: txt })
+        con.set_child(lab)
         /* TODO clickable workspaces, maybe scrollable
         addClickEvent(con, function(wsindex){
             return function(){
-                global.screen.get_workspace_by_index(wsindex).activate(1);
+                global.screen.get_workspace_by_index(wsindex).activate(1)
             }
-        }(i));
+        }(i))
         */
-        topBox.add_actor(con);
+        topBox.add_actor(con)
     }
 
     // Add window title, works with StatusTitleBar extension
-    let con = new St.Bin({ style_class: "hipster-separator" });
-    let lab = new St.Label({ text: "::" });
-    con.set_child(lab);
-    topBox.add_actor(con);
+    let con = new St.Bin({ style_class: "hipster-separator" })
+    let lab = new St.Label({ text: "::" })
+    con.set_child(lab)
+    topBox.add_actor(con)
 
-    btn.actor.add_actor(topBox);
+    btn.actor.add_actor(topBox)
 
-    return btn;
+    return btn
+}
+
+// refresh :: IO ()
+function refresh(){
+    log("-- hipster: Change!")
+    if (workspaces === null) workspaces = Workspaces()
+    let obj = []
+    for (var name in workspaces) obj.push(name)
+    log(obj)
+    let ws = workspaces.actor.get_children()[0].get_children()
+    for (var i = 0; i < ws.length - 1; i++) {
+        if (ws[i].has_style_class_name("hipster-ws")
+        || ws[i].has_style_class_name("hipster-ws-current")) {
+            ws[i].remove_style_class_name("hipster-ws-current")
+            if (global.screen.get_active_workspace_index() == i)
+                ws[i].add_style_class_name("hipster-ws-current")
+        }
+    }
 }
 
 // connectAndTrack :: Widget -> String -> IO () -> IO ()
 function connectAndTrack(s, n, f){
-    let id = s.connect(n, f);
-    log(id);
+    let id = s.connect(n, f)
+    log(id)
 }
 
 // addClickEvent :: Widget -> IO () -> IO ()
 function addClickEvent(s, f){
-    s.connect("enter-event", Lang.bind(s, f));
+    s.connect("enter-event", Lang.bind(s, f))
 }
 
 // {{{ Init
 
 // | this is impure and makes me feel dirty
 // workspaces :: Widget
-let workspaces;
+let workspaces
 // appMenu :: Widget
-let appMenu;
+let appMenu
 
 // init :: IO ()
 function init(){
-    log("-- hipster: Initialising...");
-    appMenu = Main.panel.statusArea.appMenu;
-    workspaces = Workspaces();
-    log("-- hipster: Workspaces made!");
+    log("-- hipster: Initialising...")
+    appMenu = Main.panel.statusArea.appMenu
+    workspaces = Workspaces()
+    log("-- hipster: Workspaces made!")
     connectAndTrack( global.window_manager
                    , "switch-workspace"
-                   , Lang.bind(workspaces, function(){
-                        log("-- hipster: change!");
-                        // wtf how does this even hapen ?!?!?!?!!!!
-                        if (workspaces === undefined) {
-                            workspaces = Workspaces();
-                            enable()
-                        } else {
-                            disable();
-                            workspaces = Workspaces();
-                            enable();
-                        }
-                     })
-                   );
+                   , Lang.bind(workspaces, refresh)
+                   )
     /*
     connectAndTrack( global.display
                    , "window-marked-urgent"
@@ -133,29 +156,34 @@ function init(){
 
 // enable :: IO ()
 function enable(){
+    if (workspaces === null) {
+        log("-- hipster: uh-oh!")
+        init()
+    }
     //Main.panel._leftBox.remove_actor(appMenu.actor.get_parent())
-    Main.panel.statusArea.appMenu = workspaces;
-    let index = Main.panel._leftBox.get_children().length;
-    Main.panel._leftBox.insert_child_at_index(workspaces.actor.get_parent(), index - 1);
+    Main.panel.statusArea.appMenu = workspaces
+    let index = Main.panel._leftBox.get_children().length
+    Main.panel._leftBox.insert_child_at_index(workspaces.actor.get_parent(), index - 1)
 
-    //Main.panel.addToStatusArea("hipsterWorkspaces", workspaces);
-    log("-- hipster: Enabled!");
+    //Main.panel.addToStatusArea("hipsterWorkspaces", workspaces)
+    log("-- hipster: Enabled!")
 }
 
 // disable :: IO ()
 function disable(){
-    Main.panel.menuManager.removeMenu(workspaces.menu);
-    Main.panel._leftBox.remove_actor(workspaces.actor.get_parent());
-    workspaces.destroy();
-    Main.panel.statusArea.appMenu = appMenu;
-    let index = Main.panel._leftBox.get_children().length;
-    Main.panel._leftBox.insert_child_at_index(appMenu.actor.get_parent(), index);
+    if (workspaces === null) log("-- hipster: Scream!")
+    Main.panel.menuManager.removeMenu(workspaces.menu)
+    Main.panel._leftBox.remove_actor(workspaces.actor.get_parent())
+    workspaces.destroy()
+    Main.panel.statusArea.appMenu = appMenu
+    let index = Main.panel._leftBox.get_children().length
+    Main.panel._leftBox.insert_child_at_index(appMenu.actor.get_parent(), index)
 
-    workspaces = undefined;
-    log("-- hipster: Disabled!");
+    workspaces = null
+    log("-- hipster: Disabled!")
 }
 
 // }}}
 
-// vim: noexpandtab ts=4 sw=4
+// vim: expandtab ts=4 sw=4
 
